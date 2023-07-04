37e0ae220863ffac677c6989e8e2684075a25f7d
mecodeur <mehdi.el2023@icloud.com>
2023-07-04 13:06:19 +0100

    DioError Deprecated, change it with DioException

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: AppConstant.appName,
-      locale: const Locale('ar'),
+      //locale: const Locale('ar'),
       localizationsDelegates:   [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
